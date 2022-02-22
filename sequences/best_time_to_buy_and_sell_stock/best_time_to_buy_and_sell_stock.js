c351a61fb2b4ce0e5343d62801c6b50a60ecac3b
Chris Nguyen <nguyenchris27@outlook.com>
2022-02-22 09:50:39 -0800

    best time to buy and sell stock commit

---
+++ sequences/best_time_to_buy_and_sell_stock/best_time_to_buy_and_sell_stock.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+ const maxProfit = function(prices) {
+    let minBuyPrice = Infinity;
+    let maxProfit = 0;
+    
+    for (i = 0; i < prices.length; i++) {
+        if (prices[i] < minBuyPrice) {
+            minBuyPrice = prices[i]
+        } else if (prices[i] - minBuyPrice > maxProfit) {
+            maxProfit = prices[i] - minBuyPrice;
+        }
+    } return maxProfit
+};
