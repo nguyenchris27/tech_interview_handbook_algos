0b4382dfb93e90ce3187f0e9b83da967ed3a7cd7
Chris Nguyen <nguyenchris27@outlook.com>
2022-02-21 20:08:27 -0800

    added comments

--- sequences/contains_duplicate/contains_duplicate.js
+++ sequences/contains_duplicate/contains_duplicate.js
@@ -4,22 +4,23 @@
  */
 
 // USING HASHMAP //
-
-//  const containsDuplicate = function(nums) {
-//     let hashmap = {};
-//     for(let i = 0; i < nums.length; i++) {
-//         if(hashmap[nums[i]]) {
-//             return true;
-//         } hashmap[nums[i]] = 1;
-//     } return false;
-// };
-
-// USING SORT
-const containsDuplicate = function(nums) {
-    nums.sort((a, b) => a - b)
+// Faster runtime, worse space complexity
+ const containsDuplicate = function(nums) {
+    let hashmap = {};
     for(let i = 0; i < nums.length; i++) {
-        if (i > 0 && nums[i] === nums[i-1]) {
+        if(hashmap[nums[i]]) {
             return true;
-        }
+        } hashmap[nums[i]] = 1;
     } return false;
 };
+
+// USING SORT
+// Slower runtime, better space complexity
+// const containsDuplicate = function(nums) {
+//     nums.sort((a, b) => a - b)
+//     for(let i = 0; i < nums.length; i++) {
+//         if (i > 0 && nums[i] === nums[i-1]) {
+//             return true;
+//         }
+//     } return false;
+// };
