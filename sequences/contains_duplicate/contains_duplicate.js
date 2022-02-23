b77be456e5100d19d9464e7c4ba8bdab4f6674ba
Chris Nguyen <nguyenchris27@outlook.com>
2022-02-23 11:03:55 -0800

    fixed indentation

--- sequences/contains_duplicate/contains_duplicate.js
+++ sequences/contains_duplicate/contains_duplicate.js
@@ -5,10 +5,10 @@
 
 // USING HASHMAP //
 // Faster runtime, worse space complexity
-const containsDuplicate = function(nums) {
+const containsDuplicate = function (nums) {
     let hashmap = {};
-    for(let i = 0; i < nums.length; i++) {
-        if(hashmap[nums[i]]) {
+    for (let i = 0; i < nums.length; i++) {
+        if (hashmap[nums[i]]) {
             return true;
         } hashmap[nums[i]] = 1;
     } return false;
